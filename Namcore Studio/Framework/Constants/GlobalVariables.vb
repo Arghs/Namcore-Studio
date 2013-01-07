3f8540bf7849d563e66b2a5b2830b9ad0d3835d0
megasus <geslauncher@web.de>
2013-01-07 21:10:19 +0100

    Update 07/01/13

--- Namcore Studio/Framework/Constants/GlobalVariables.vb
+++ Namcore Studio/Framework/Constants/GlobalVariables.vb
@@ -1,4 +1,5 @@
 ﻿Public Class GlobalVariables
-    Public Shared type As New EventTypes.Complex
 
+    Public Shared eventlog As String
+    Public Shared eventlog_full As String
 End Class
--- Namcore Studio/Framework/Logging/EventLogging.vb
+++ Namcore Studio/Framework/Logging/EventLogging.vb
@@ -1,6 +1,21 @@
-﻿Public Class EventLogging
+﻿Imports Namcore_Studio.GlobalVariables
 
-    Public Shared Sub LogAppend(ByVal EventType As String)
+Public Class EventLogging
 
+    Public Shared Sub LogAppend(ByVal _event As String, ByVal location As String, Optional userOut As Boolean = False, Optional iserror As Boolean = False)
+        If iserror = False Then
+            If userOut = True Then
+                eventlog = eventlog & vbNewLine & "[" & Now.TimeOfDay.ToString & "]" & _event
+                eventlog_full = eventlog_full & vbNewLine & "[" & Date.Today.ToString & " " & Now.TimeOfDay.ToString & "]" & _event
+            Else
+
+            End If
+        Else
+
+        End If
+    End Sub
+
+    Public Shared Sub LogClear()
+        eventlog = ""
     End Sub
 End Class
