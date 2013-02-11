dc344c9186756b7f5a120dd924551a7e60063035
megasus <geslauncher@web.de>
2013-02-11 21:02:56 +0100

    Added copyright info to new files

--- Namcore Remote Server/Logging.vb
+++ Namcore Remote Server/Logging.vb
@@ -1,4 +1,26 @@
-﻿Public Class Logging
+﻿'+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+'* Copyright (C) 2013 Namcore Studio <https://github.com/megasus/Namcore-Studio>
+'*
+'* This program is free software; you can redistribute it and/or modify it
+'* under the terms of the GNU General Public License as published by the
+'* Free Software Foundation; either version 2 of the License, or (at your
+'* option) any later version.
+'*
+'* This program is distributed in the hope that it will be useful, but WITHOUT
+'* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+'* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+'* more details.
+'*
+'* You should have received a copy of the GNU General Public License along
+'* with this program. If not, see <http://www.gnu.org/licenses/>.
+'*
+'* Developed by Alcanmage/megasus
+'*
+'* //FileInfo//
+'*      /Filename:      Logging
+'*      /Description:   I guess it logs sth.
+'+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Public Class Logging
     Public Shared Sub LogMessage(ByVal Message As String, ByVal LogFileName As String)
         Try
             Dim oStream As IO.StreamWriter = System.IO.File.AppendText(LogFileName + ".log")
