37aaf6b87fcc123ad2db2db92d5414441aadf687
Alcanmage <geslauncher@gmx.de>
2013-04-07 00:17:22 +0200

    Update 07/04/13

---
+++ Namcore Studio/Framework/Armory/Parser/ItemParser.vb
@@ -0,0 +1,159 @@
+﻿Imports Namcore_Studio.EventLogging
+Imports Namcore_Studio.Conversions
+Imports Namcore_Studio.SpellItem_Information
+Imports Namcore_Studio.Basics
+Public Class ItemParser
+    Public Shared Sub loadItems(ByVal source As String, ByVal setId As Integer)
+        Dim itemguid As Integer
+        Dim slotname As String
+        Dim itementry As Integer
+        Dim itemslot As Integer
+        Dim loopcounter As Integer = 0
+        Dim slot_st
+        Do
+            Try
+              
+                If itemslot > 18 Then
+                    loopcounter += 1
+                    Continue Do
+                End If
+                Select Case itemslot
+                    Case 0
+                        slotname = "head"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 1
+                        slotname = "neck"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 2
+                        slotname = "shoulder"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 3
+                        slotname = "shirt"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 4
+                        slotname = "chest"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 5
+                        slotname = "waist"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 6
+                        slotname = "legs"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 7
+                        slotname = "feet"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 8
+                        slotname = "wrists"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 9
+                        slotname = "hands"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 10
+                        slotname = "finger1"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 11
+                        slotname = "finger2"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 12
+                        slotname = "trinket1"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 13
+                        slotname = "trinket2"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 14
+                        slotname = "back"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 15
+                        slotname = "main"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 16
+                        slotname = "off"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case 17
+                        'slot 17 has been removed as of patch 5.0
+                    Case 18
+                        slotname = "tabard"
+                        If itementry > 1 Then
+                            SetTemporaryCharacterInformation("@character_" & slotname & "Id", itementry.ToString(), tar_setId)
+                            GetItemStats(itemguid, slotname, tar_setId)
+                            LoadWeaponType(itementry, tar_setId) : End If
+                    Case Else : End Select
+
+            Catch ex As Exception
+                LogAppend("Something went wrong! -> Exception is: ###START###" & ex.ToString() & "###END###", "CharacterArmorHandler_loadAtArcemu", False, True)
+                loopcounter += 1
+                Continue Do
+            End Try
+            loopcounter += 1
+        Loop Until loopcounter = entrycount
+
+    End Sub
+    Private Function getItemInfo(ByVal slot As Integer, ByVal slotname As String, ByVal sourceCode As String) As Item
+        Dim relevantItemContext As String = splitString(sourceCode, "<div data-id=""" & slot.ToString & """ data-type=", "</div>")
+        Dim charItem As New Item
+        '//Loading ID
+        charItem.id = TryInt(splitString(relevantItemContext, "/item/", """ class=""item"""))
+        If charItem.id = Nothing Then Return Nothing '//Item ID not found
+        '//Loading Name
+        charItem.name = splitString(relevantItemContext, "<span class=""name-shadow"">", "</span>")
+        '//Loading Sockets
+        Dim socketContext As String
+        If relevantItemContext.Contains("<span class=""sockets"">") Then
+            'sockets available
+            socketContext = splitString(sourceCode & "</div>", "<span class=""sockets"">", "</div>")
+            Dim socketCount As Integer = UBound(socketContext.Split("socket-"))
+
+        End If
+    End Function
+End Class
---
+++ Namcore Studio/Framework/Constants/Item.vb
@@ -0,0 +1,46 @@
+﻿'+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+'* Copyright (C) 2013 Namcore Studio <https://github.com/megasus/Namcore-Studio>
+'*
+'* This program is free software; you can redistribute it and/or modify it
+'* under the terms of the GNU General Public License as published by the
+'* Free Software Foundation; either version 2 of the License, or (at your
+'* option) any later version.
+'*
+'* This program is distributed in the hope that it will be useful, but WITHOUT
+'* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+'* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+'* more details.
+'*
+'* You should have received a copy of the GNU General Public License along
+'* with this program. If not, see <http://www.gnu.org/licenses/>.
+'*
+'* Developed by Alcanmage/megasus
+'*
+'* //FileInfo//
+'*      /Filename:      Item
+'*      /Description:   Item Object - item information class
+'+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+Public Class Item
+
+    Public id As Integer
+    Public name As String
+    Public rarity As Integer '1=poor;2=common;3=uncommon;3=rare;4=epic;5=legendary;6=artifact/heirloom
+    Public slotname As String
+    Public socket1_id As Integer
+    Public socket2_id As Integer
+    Public socket3_id As Integer
+    Public socket1_name As String
+    Public socket2_name As String
+    Public socket3_name As String
+    Public enchantment_type As Integer '1=spell;2=item
+    Public enchantment_id As Integer
+    Public enchantment_name As String
+    Public image As Image
+    Public Sub New()
+
+    End Sub
+
+
+End Class
