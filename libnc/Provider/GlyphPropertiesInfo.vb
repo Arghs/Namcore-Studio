893249a351b2a952b8e2ac28889f20e53feeb17e
megasus <geslauncher@web.de>
2013-10-12 23:41:35 +0200

    Fixed some copyright headers

--- libnc/Provider/GlyphPropertiesInfo.vb
+++ libnc/Provider/GlyphPropertiesInfo.vb
@@ -13,7 +13,7 @@
 '*
 '* You should have received a copy of the GNU General Public License along
 '* with this program. If not, see <http://www.gnu.org/licenses/>.
-'*F:\Projekte\Visual Studio\Namcore-Studio\Namcore Studio Framework\Framework\Functions\SpellItemInformation.vb
+'*
 '* Developed by Alcanmage/megasus
 '*
 '* //FileInfo//
