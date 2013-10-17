bc4431c9c282e69ed0a9aec1e278a4493810e40e
megasus <geslauncher@web.de>
2013-10-17 20:27:24 +0200

    [45952] Updated Build version

--- Namcore Studio/My Project/AssemblyInfo.vb
+++ Namcore Studio/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.10.45951")> 
-<Assembly: AssemblyFileVersion("0.0.10.45951")> 
+<Assembly: AssemblyVersion("0.0.10.45952")> 
+<Assembly: AssemblyFileVersion("0.0.10.45952")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
--- NamcoreDirectAccess/My Project/AssemblyInfo.vb
+++ NamcoreDirectAccess/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.10.38")> 
-<Assembly: AssemblyFileVersion("0.0.10.38")> 
+<Assembly: AssemblyVersion("0.0.10.39")> 
+<Assembly: AssemblyFileVersion("0.0.10.39")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
