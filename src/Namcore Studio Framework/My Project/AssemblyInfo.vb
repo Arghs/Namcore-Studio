d61690a69fb7176f8913225276b3eb04708880b9
megasus <geslauncher@web.de>
2013-12-22 13:11:41 +0100

    [46046/46079] Bumped build version

--- src/Namcore Studio Framework/My Project/AssemblyInfo.vb
+++ src/Namcore Studio Framework/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.12.46076")> 
-<Assembly: AssemblyFileVersion("0.0.12.46076")> 
+<Assembly: AssemblyVersion("0.0.12.46079")> 
+<Assembly: AssemblyFileVersion("0.0.12.46079")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
--- src/Namcore Studio/My Project/AssemblyInfo.vb
+++ src/Namcore Studio/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.12.46043")> 
-<Assembly: AssemblyFileVersion("0.0.12.46043")> 
+<Assembly: AssemblyVersion("0.0.12.46046")> 
+<Assembly: AssemblyFileVersion("0.0.12.46046")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
