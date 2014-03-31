3f7326d49c5de97ea90e139d04ab2b89750633fe
megasus <geslauncher@web.de>
2014-03-31 17:37:00 +0200

    [46291/46503] Bumped version - release v.0.1.1

--- src/Namcore Studio/My Project/AssemblyInfo.vb
+++ src/Namcore Studio/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Resources
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.1.46290")> 
-<Assembly: AssemblyFileVersion("0.1.1.46290")> 
+<Assembly: AssemblyVersion("0.1.1.46291")> 
+<Assembly: AssemblyFileVersion("0.1.1.46291")> 
 
 <Assembly: NeutralResourcesLanguage("en-US")>
