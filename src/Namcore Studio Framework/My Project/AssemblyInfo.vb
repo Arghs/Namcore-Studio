2848bc629a76bd6aed0b22808c97dad71b51c1de
megasus <geslauncher@web.de>
2014-04-20 14:51:08 +0200

    [46334/46534] Updated user guide

--- src/Namcore Studio Framework/My Project/AssemblyInfo.vb
+++ src/Namcore Studio Framework/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Resources
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.2.46533")> 
-<Assembly: AssemblyFileVersion("0.1.2.46533")> 
+<Assembly: AssemblyVersion("0.1.2.46534")> 
+<Assembly: AssemblyFileVersion("0.1.2.46534")> 
 
 <Assembly: NeutralResourcesLanguage("en-US")>
