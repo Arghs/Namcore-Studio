34e66938d5c42939696520e597affb8b48b828f6
Alcanmage <geslauncher@gmx.de>
2014-05-30 16:07:38 +0200

    [46345/46542] Bumped version (30 days late)

--- src/Namcore Studio/My Project/AssemblyInfo.vb
+++ src/Namcore Studio/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Resources
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.2.46344")> 
-<Assembly: AssemblyFileVersion("0.1.2.46344")> 
+<Assembly: AssemblyVersion("0.1.3.46345")> 
+<Assembly: AssemblyFileVersion("0.1.3.46345")> 
 
 <Assembly: NeutralResourcesLanguage("en-US")>
