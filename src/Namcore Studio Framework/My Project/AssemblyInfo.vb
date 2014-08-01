4d34ef8c99db703feba52ea29af7474367240204
alcanmage <geslauncher@gmx.de>
2014-08-01 09:36:21 +0200

    [46356/46557] Bumped version

--- src/Namcore Studio Framework/My Project/AssemblyInfo.vb
+++ src/Namcore Studio Framework/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Resources
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.5.46556")> 
-<Assembly: AssemblyFileVersion("0.1.5.46556")> 
+<Assembly: AssemblyVersion("0.1.6.46557")> 
+<Assembly: AssemblyFileVersion("0.1.6.46557")> 
 
 <Assembly: NeutralResourcesLanguage("en-US")>
--- src/Namcore Studio/My Project/AssemblyInfo.vb
+++ src/Namcore Studio/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Resources
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.5.46355")> 
-<Assembly: AssemblyFileVersion("0.1.5.46355")> 
+<Assembly: AssemblyVersion("0.1.6.46356")> 
+<Assembly: AssemblyFileVersion("0.1.6.46356")> 
 
 <Assembly: NeutralResourcesLanguage("en-US")>
