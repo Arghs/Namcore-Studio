54fb515334ca6e576afcd998b1489092a4d381f1
megasus <geslauncher@web.de>
2014-11-08 17:49:36 +0100

    Pushed build

--- src/Namcore Studio Framework/My Project/AssemblyInfo.vb
+++ src/Namcore Studio Framework/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Resources
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.7.46564")> 
-<Assembly: AssemblyFileVersion("0.1.7.46564")> 
+<Assembly: AssemblyVersion("0.1.7.46565")> 
+<Assembly: AssemblyFileVersion("0.1.7.46565")> 
 
 <Assembly: NeutralResourcesLanguage("en-US")>
